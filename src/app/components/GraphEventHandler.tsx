b90c06f998663413133ec41ee176257f6eb7f574
Tyler Olson <tydotg@gmail.com>
2022-12-26 17:15:58 -0600

    better hover ui

--- src/app/components/GraphEventHandler.tsx
+++ src/app/components/GraphEventHandler.tsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from 'react';
-import { EventHandlers } from "@react-sigma/core";
+import { EventHandlers, useCamera } from "@react-sigma/core";
 import { useSigma, useRegisterEvents, useSetSettings } from "@react-sigma/core";
+import { Attributes } from 'graphology-types';
 
 import "@react-sigma/core/lib/react-sigma.min.css";
 
-import { Attributes } from 'graphology-types';
 
 export const GraphEvents = () => {
   const sigma = useSigma();
   const setSettings = useSetSettings();
   const registerEvents: (eventHandlers: Partial<EventHandlers>) => void = useRegisterEvents();
+  const { gotoNode } = useCamera();
 
   const [selectedNode, setSelectedNode] = useState<string | null>(null);
   const [hoveredNode, setHoveredNode] = useState<string | null>(null);
@@ -18,19 +19,24 @@ export const GraphEvents = () => {
   useEffect(() => {
     registerEvents({
       clickNode: (event) => {
-        setSelectedNode(event.node);
+        if (selectedNode === event.node) {
+          setSelectedNode(null);
+        } else {
+          setSelectedNode(event.node);
+          gotoNode(event.node);
+        }
         setHoveredNode(null);
       },
       enterNode: (event) => setHoveredNode(event.node),
       leaveNode: () => setHoveredNode(null),
     });
-  }, [registerEvents]);
+  }, [registerEvents, selectedNode, hoveredNode]);
 
   useEffect(() => {
     setSettings({
       // TODO: think about intented UI here, kind of messy interactions right now
       nodeReducer: (node: string, data: Attributes): Attributes => {
-        // if nothing selected or hovered, quick return
+        // if nothing selected or hovered, quick return default
         if (!hoveredNode && !selectedNode) return data;
 
         const nodeIsSelected = selectedNode === node;
@@ -41,41 +47,39 @@ export const GraphEvents = () => {
         
         // check neighbors...
         const graph = sigma.getGraph();
-        if (selectedNode && graph.neighbors(selectedNode).includes(node)) return { ...data, highlighted: true };
+
+        if (selectedNode && graph.neighbors(selectedNode).includes(node)) {
+          return { 
+            ...data, 
+            highlighted: true,
+          };
+        }
         if (hoveredNode && graph.neighbors(hoveredNode).includes(node)) return { ...data, highlighted: true };
 
-        // otherwise, nothing
+        // otherwise, de-emphasize node
         return {
           ...data, 
           color: '#E2E2E2', 
-          // labelWeight: 'light',
           label: null,
           highlighted: false,
-          // size: data.size - 2,
-          // labelSize: 12, // default 14
         };
       },
       edgeReducer: (edge: string, data: Attributes): Attributes => {
-          // if nothing selected or hovered, quick return
-          if (!hoveredNode && !selectedNode) return data;
+        // if nothing selected or hovered, quick return default
+        if (!hoveredNode && !selectedNode) return data;
 
+        // check neighbors
         const graph = sigma.getGraph();
 
-        // check neighbors...
-        // if (selectedNode || hoveredNode) {
         const isSelectedNeighbor = selectedNode && graph.extremities(edge).includes(selectedNode);
         const isHoveredNeighbor = hoveredNode && graph.extremities(edge).includes(hoveredNode);
 
         if (!isSelectedNeighbor && !isHoveredNeighbor) return { ...data, hidden: true };
 
-        // }
-        // if (selectedNode && !graph.extremities(edge).includes(selectedNode)) return { ...data, hidden: true };
-        // if (hoveredNode && !graph.extremities(edge).includes(hoveredNode)) return { ...data, hidden: true };
-
         return data;
       }
     });
-  }, [hoveredNode, setSettings, sigma]);
+  }, [hoveredNode, selectedNode, setSettings, sigma]);
 
 
   return null;
