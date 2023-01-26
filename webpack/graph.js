1fed1405f332ce9f1f40b2ae5807bffb8229e441
Tyler Olson <tydotg@gmail.com>
2023-01-26 13:14:47 -0800

    tidy up build files

--- webpack/graph.js
+++ webpack/graph.js
@@ -1,5 +1,4 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require('path'); // eslint-disable-line @typescript-eslint/no-var-requires
 
 module.exports = {
   entry: path.resolve(__dirname, '../src/graph/index.ts'),
@@ -9,20 +8,6 @@ module.exports = {
   output: {
     filename: 'graph.bundle.js',
     path: path.resolve(__dirname, '../dist')
-  },
-  module: { 
-    rules: [
-      {
-        test: /\.node$/,
-        use: 'node-loader'
-      }
-    ],
-  },
-  plugins: [
-    new webpack.NormalModuleReplacementPlugin(/^vertx/, '@vertx/core')
-  ],
-  externals : { 
-    // canvas: {} 
   },
   resolve: {
     extensions: ['.js', '.ts'],
