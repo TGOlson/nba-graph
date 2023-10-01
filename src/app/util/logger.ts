afe1a757c45d646e955c7b46792b495ef926eb5b
Tyler Olson <tydotg@gmail.com>
2023-09-30 21:14:55 -0500

    toggle logging for env

--- src/app/util/logger.ts
+++ src/app/util/logger.ts
@@ -1,4 +1,4 @@
 export const logDebug = (...args: any[]) => // eslint-disable-line
-  process.env.NODE_ENV === 'production' 
-    ? console.debug('[debug]', ...args)  // eslint-disable-line
-    : console.log('[debug]', ...args); // eslint-disable-line
+  process.env.NODE_ENV === 'development' 
+    ? console.log('[debug]', ...args)    // eslint-disable-line
+    : console.debug('[debug]', ...args); // eslint-disable-line
