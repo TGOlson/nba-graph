9bb47260d49b146316f5b214b6abdc131274cffc
Tyler Olson <tydotg@gmail.com>
2023-10-06 21:31:49 -0500

    remove git lfs

--- archive-data.sh
+++ archive-data.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-tar -C data -czf data/img.tar.gz img/
-tar -C data -czf data/parsed.tar.gz parsed/
-tar -C data -czf data/www.basketball-reference.com.tar.gz www.basketball-reference.com/
+tar -czf data.tar.gz data/
