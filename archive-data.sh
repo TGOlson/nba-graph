ed8b56c5dc4dbf8ab2e89be89c600305a68bd1a9
Tyler Olson <tydotg@gmail.com>
2023-10-06 21:06:26 -0500

    commit graph and sprites

---
+++ archive-data.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+tar -C data -czf data/img.tar.gz img/
+tar -C data -czf data/parsed.tar.gz parsed/
+tar -C data -czf data/www.basketball-reference.com.tar.gz www.basketball-reference.com/
